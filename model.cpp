b13f115fa5a62bf34a62873cb49386d4576afd09
Christopher Dembia <cld72@cornell.edu>
2013-12-18 23:24:46 -0800

    Try to get CMake to find OpenSim.
    Having an issue with the RegisterTypes stuff.

---
+++ model.cpp
@@ -0,0 +1,14 @@
+
+#include <OpenSim/OpenSim.h>
+
+/**
+ * Creates an OpenSim model of a three-legged animal. The animal has only one
+ * hind leg, centered mediolaterally.
+ * */
+int main(int argc, char * argv[])
+{
+    OpenSim::Model m;
+    std::cout << "Hello, world!" << std::endl;
+    return EXIT_SUCCESS;
+}
+
